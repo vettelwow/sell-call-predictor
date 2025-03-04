7504b52f3da3decee4d29ade8a3066f726bf6628
vettelwow <vettelwow4@gmail.com>
2025-03-04 09:40:42 +0800

    Update sell_call_tool.py

--- sell_call_tool.py
+++ sell_call_tool.py
@@ -3,8 +3,13 @@ import numpy as np
 import pickle
 
 # 加載機器學習模型
-with open('sell_call_model.pkl', 'rb') as model_file:
+import os
+current_dir = os.path.dirname(os.path.abspath(__file__))
+model_path = os.path.join(current_dir, "sell_call_model.pkl")
+
+with open(model_path, 'rb') as model_file:
     model = pickle.load(model_file)
+
 
 # 應用介面
 st.title('SELL CALL 成功率預測工具')
